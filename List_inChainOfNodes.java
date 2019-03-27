68b6bfcb8ccc5cdf5a6d2c290bbadf919e8c7224
Amy Lei <amy.lei2001@yahoo.com>
2019-03-27 19:05:30 -0400

    hw59

--- List_inChainOfNodes.java
+++ List_inChainOfNodes.java
@@ -64,76 +64,78 @@ public class List_inChainOfNodes{
        
         return true;
      }
-	 
-	/**
-	 @return value corresponding to the given index
-	 */
-	 public Object get(int index){
-		 return getNode(index).getCargoReference();
-		 
-	 }
+     
+    /**
+     @return value corresponding to the given index
+     */
+     public Object get(int index){
+         return getNode(index).getCargoReference();
+         
+     }
 
-	/**
-	 Helper function for add and remove
-	 @return Node corresponding to the given index
-	 */
-	 private Node getNode(int index){
-		 Node curNode = headReference;
-		 for( int el = 0; ; el++){
-			 if (el == index) return curNode;
-			 curNode = curNode.getReferenceToNextNode();
-			
-		 }
-		 
-	 }
-	 
-	/**
-	  Append @value to the list at the given index. 
-	  
-	  @return true, in keeping with conventions. 
-	  
-	 */
-	public boolean add(int index, Object val){
-	
-		// if index == 0 use addAsHead
-		if(index==0) addAsHead( val);
-		
-		else{ 
-			// store former element at index 
-			Node nodeBefore = getNode(index -1);
-			Node nodeToBeShifted = nodeBefore.getReferenceToNextNode();
-		
-		
-			// create a Node using given value and set to it as next Node of nodeBefore
-			Node addition = new Node( val, nodeToBeShifted);
-			nodeBefore.setReferenceToNextNode( addition);
-		}
-		return true;
-	}
-	
-	/**
-	  Sets @value at given index
-	  
-	  @return old value, in keeping with conventions
-	 */
-	public Node set( int index, Object val){
-		// store value of succeeding node and replaced node
-        	Node replacedNode = getNode(index);
-        	Node succeedingNode = replacedNode.getReferenceToNextNode();
+    /**
+     Helper function for add and remove
+     @return Node corresponding to the given index
+     */
+    private Node getNode(int index){
+        Node node;
+         int counter;
+        for( counter = 0, node = headReference
+            ; counter < index
+            ; counter++, node = node.getReferenceToNextNode()
+            )
+            ;
+        return node;
+
+     }
+     
+    /**
+      Append @value to the list at the given index. 
+      
+      @return true, in keeping with conventions. 
+      
+     */
+    public boolean add(int index, Object val){
+    
+    
+        // if index == 0 use addAsHead
+        if(index==0) addAsHead( val);
         
-        	// create new Node using succeedingNode as nextRef
-       		 Node addition = new Node( val, succeedingNode);
+        else{ 
+            // store former element at index 
+            Node proceedingNode = getNode(index -1);
+            Node nodeAtIndex = proceedingNode.getReferenceToNextNode();
         
-        	// if setting @index 0, make this new node the headref
-       		 if (index == 0) headReference = addition;
-        	// otherwise have it be the nextRef of the proceeding node
-        	else { getNode(index-1).setReferenceToNextNode( addition);}
-	
+        
+            // create a Node using given value and set to it as next Node of proceedingNode
+            Node addition = new Node( val, nodeAtIndex);
+            proceedingNode.setReferenceToNextNode( addition);
+        }
+        return true;
+    }
+    
+    /**
+      Sets @value at given index
+      
+      @return old value, in keeping with conventions
+     */
+    public Node set( int index, Object val){
+        // store value of succeeding node and replaced node
+        Node replacedNode = getNode(index);
+        
+        // create new Node using the replacedNode's nextRef
+        Node addition = new Node( val, replacedNode.getReferenceToNextNode());
+        
+        // if setting @index 0, make this new node the headref
+        if (index == 0) headReference = addition;
+        // otherwise have it be the nextRef of the proceeding node
+        else { getNode(index-1).setReferenceToNextNode( addition);}
+    
         
         //return old value by convention
-		return replacedNode;
-		
-	}
+        return replacedNode;
+        
+    }
     
     /** 
       Removes @value at given index
@@ -141,18 +143,16 @@ public class List_inChainOfNodes{
       @return value removed, in keeping with conventions
      */
      
-    public Node remove( int index){
-        // save old succeeding Node
-        Node removedNode = getNode(index);
-        Node succeedingNode = removedNode.getReferenceToNextNode();
+    public void remove( int index){
+        // save the nextRef of the node to be removed
+        Node nextNode = getNode(index + 1);
+
+        // if removing from the head, set nextNode as new headRef
+        if (index==0) headReference = nextNode;
+        else 
+            // otherwise set the nextRef of the preceeding node to nextNode
+            getNode(index-1).setReferenceToNextNode( nextNode);
         
-        // if removing from the head, set succeeding as new headRef
-        if (index==0) headReference = succeedingNode;
-        else {
-            // set the nextRef of the preceeding node to the succeedingNode
-            getNode(index-1).setReferenceToNextNode( succeedingNode);
-        }
-        
-        return removedNode;
+
     }
 }
